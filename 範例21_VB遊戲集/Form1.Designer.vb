07b96d7a70d4be2c33d9ad4ca2f0a1f8f91094da
loshumey <shumey1510@gmail.com>
2018-12-25 10:59:01 +0800

    加入專案檔案。

---
+++ 範例21_VB遊戲集/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.猜數字ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.終極密碼 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.幾A幾BToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.結束ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mylist = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Pic = New System.Windows.Forms.PictureBox()
+        Me.LabelOut = New System.Windows.Forms.Label()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.Pic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Font = New System.Drawing.Font("Microsoft JhengHei UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.猜數字ToolStripMenuItem, Me.結束ToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(8, 3, 0, 3)
+        Me.MenuStrip1.Size = New System.Drawing.Size(745, 35)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "猜數字"
+        '
+        '猜數字ToolStripMenuItem
+        '
+        Me.猜數字ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.終極密碼, Me.幾A幾BToolStripMenuItem})
+        Me.猜數字ToolStripMenuItem.Name = "猜數字ToolStripMenuItem"
+        Me.猜數字ToolStripMenuItem.Size = New System.Drawing.Size(104, 29)
+        Me.猜數字ToolStripMenuItem.Text = "遊戲選擇"
+        '
+        '終極密碼
+        '
+        Me.終極密碼.Name = "終極密碼"
+        Me.終極密碼.Size = New System.Drawing.Size(164, 30)
+        Me.終極密碼.Text = "終極密碼"
+        '
+        '幾A幾BToolStripMenuItem
+        '
+        Me.幾A幾BToolStripMenuItem.Name = "幾A幾BToolStripMenuItem"
+        Me.幾A幾BToolStripMenuItem.Size = New System.Drawing.Size(164, 30)
+        Me.幾A幾BToolStripMenuItem.Text = "幾A幾B"
+        '
+        '結束ToolStripMenuItem
+        '
+        Me.結束ToolStripMenuItem.Name = "結束ToolStripMenuItem"
+        Me.結束ToolStripMenuItem.Size = New System.Drawing.Size(64, 29)
+        Me.結束ToolStripMenuItem.Text = "結束"
+        '
+        'mylist
+        '
+        Me.mylist.FormattingEnabled = True
+        Me.mylist.ItemHeight = 34
+        Me.mylist.Location = New System.Drawing.Point(23, 98)
+        Me.mylist.Name = "mylist"
+        Me.mylist.Size = New System.Drawing.Size(266, 480)
+        Me.mylist.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(16, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(211, 35)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "遊戲過程與提示"
+        '
+        'Pic
+        '
+        Me.Pic.Location = New System.Drawing.Point(372, 112)
+        Me.Pic.Name = "Pic"
+        Me.Pic.Size = New System.Drawing.Size(263, 247)
+        Me.Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Pic.TabIndex = 3
+        Me.Pic.TabStop = False
+        '
+        'LabelOut
+        '
+        Me.LabelOut.AutoSize = True
+        Me.LabelOut.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.LabelOut.Location = New System.Drawing.Point(366, 381)
+        Me.LabelOut.Name = "LabelOut"
+        Me.LabelOut.Size = New System.Drawing.Size(211, 35)
+        Me.LabelOut.TabIndex = 4
+        Me.LabelOut.Text = "遊戲過程與提示"
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "厲害.jpeg")
+        Me.ImageList1.Images.SetKeyName(1, "中等.png")
+        Me.ImageList1.Images.SetKeyName(2, "笨.jpg")
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 34.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(745, 629)
+        Me.Controls.Add(Me.LabelOut)
+        Me.Controls.Add(Me.Pic)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.mylist)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("微軟正黑體", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(5, 7, 5, 7)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.Pic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents 猜數字ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 終極密碼 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 幾A幾BToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents 結束ToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mylist As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Pic As System.Windows.Forms.PictureBox
+    Friend WithEvents LabelOut As System.Windows.Forms.Label
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+
+End Class
---
+++ 範例21_VB遊戲集/Form1.vb
@@ -0,0 +1,155 @@
+﻿Public Class Form1
+
+
+    Private Sub 結束ToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles 結束ToolStripMenuItem.Click
+        End
+
+    End Sub
+
+   
+    Private Sub 終極密碼_Click(sender As System.Object, e As System.EventArgs) Handles 終極密碼.Click
+        Label1.Visible = True
+        mylist.Visible = True
+        LabelOut.Visible = True
+        Pic.Visible = True
+
+        mylist.Items.Clear()
+        LabelOut.Text = Nothing
+        Pic.Image = Nothing
+
+        '---電腦的標準答案--------
+        Randomize()
+        Dim ans As Integer
+        Dim user_ans As Integer = 0
+        Dim tot As Integer = 0
+        ans = Int(Rnd() * 101)
+        'mylist.Items.Add(ans)
+
+        '----開始猜答案-------
+        Do
+            user_ans = Val(InputBox("請輸入你的答案"))
+            tot = tot + 1
+            If user_ans > ans Then
+                mylist.Items.Add(user_ans & "-->太大了")
+            ElseIf user_ans < ans Then
+                mylist.Items.Add(user_ans & "-->太小了")
+            Else
+                mylist.Items.Add(user_ans & "-->標準答案")
+            End If
+        Loop Until user_ans = ans
+
+        Select Case tot
+            Case 1 To 3
+                LabelOut.Text = "總共猜了" & tot & "次，太厲害了"
+                Pic.Image = ImageList1.Images(0)
+            Case 4 To 7
+                LabelOut.Text = "總共猜了" & tot & "次，還可以喔"
+                Pic.Image = ImageList1.Images(1)
+            Case Else
+                LabelOut.Text = "總共猜了" & tot & "次，該補腦囉"
+                Pic.Image = ImageList1.Images(2)
+        End Select
+
+        Dim bmp As Bitmap
+        bmp = Pic.Image
+        bmp.MakeTransparent(Color.White)   'Color白色的部份透明 
+
+        'bmp.Save("Result.bmp")
+        'Pic.ImageLocation = "Result.bmp"
+
+    End Sub
+
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Label1.Visible = False
+        mylist.Visible = False
+        LabelOut.Visible = False
+        Pic.Visible = False
+    End Sub
+
+    Private Sub 幾A幾BToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles 幾A幾BToolStripMenuItem.Click
+        Label1.Visible = True
+        mylist.Visible = True
+        LabelOut.Visible = True
+        Pic.Visible = True
+
+        mylist.Items.Clear()
+        LabelOut.Text = Nothing
+        Pic.Image = Nothing
+
+        '---電腦的標準答案--------
+        Randomize()
+        Dim ans As String
+        Dim user_ans As String = ""
+        Dim A, B As Integer
+        Dim tot As Integer = 0
+        ans = c_ans()
+        mylist.Items.Add(ans)
+
+
+        '----開始猜答案-------
+        Do
+            user_ans = InputBox("請輸入你的答案")
+            tot = tot + 1
+            A = cmp_ansA(user_ans, ans)
+            B = cmp_ansB(user_ans, ans)
+
+            mylist.Items.Add(user_ans & "-->" & A & "A" & B & "B")
+        Loop Until user_ans = ans
+
+        Select Case tot
+            Case 1 To 7
+                LabelOut.Text = "總共猜了" & tot & "次，太厲害了"
+                Pic.Image = ImageList1.Images(0)
+            Case 8 To 15
+                LabelOut.Text = "總共猜了" & tot & "次，還可以喔"
+                Pic.Image = ImageList1.Images(1)
+            Case Else
+                LabelOut.Text = "總共猜了" & tot & "次，該補腦囉"
+                Pic.Image = ImageList1.Images(2)
+        End Select
+
+        Dim bmp As Bitmap
+        bmp = Pic.Image
+        bmp.MakeTransparent(Color.White)   'Color白色的部份透明 
+    End Sub
+
+    '====產生標準答案=====
+    Function c_ans() As String
+        Randomize()
+        Dim x As Integer
+        Dim ans As String = ""
+        For i = 1 To 4
+            x = Int(Rnd() * 10)
+            If InStr(ans, Trim(Str(x))) = 0 Then
+                ans = ans & x
+            Else
+                i = i - 1
+            End If
+        Next
+        Return ans
+    End Function
+
+    '====比對答案？A=====
+    Function cmp_ansA(x, y) As String
+        Dim a As Integer = 0
+        For i = 0 To 3
+            If x(i) = y(i) Then
+                a = a + 1
+            End If
+        Next
+        Return (a)
+    End Function
+
+    '====比對答案？B=====
+    Function cmp_ansB(x, y) As String
+        Dim b As Integer = 0
+        For i = 0 To 3
+            If x(i) <> y(i) And InStr(y, x(i)) > 0 Then
+                b = b + 1
+            End If
+        Next
+        Return (b)
+    End Function
+
+End Class
